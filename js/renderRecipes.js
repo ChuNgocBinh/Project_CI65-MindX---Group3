d87b4157029f913ee07080445fefa76ea2fb3c4d
Ashimory <47056302+Ashimory@users.noreply.github.com>
2021-09-14 11:25:45 +0700

    started recipe display grid

--- js/renderRecipes.js
+++ js/renderRecipes.js
@@ -1,5 +1,5 @@
 import Recipes from "./screen/allRecipes.js";
+
 let _recipes = new Recipes();
 let $display = document.querySelector('.display');
-console.log("check")
 $display.append(..._recipes.render()); 
--- js/screen/allRecipes.js
+++ js/screen/allRecipes.js
@@ -49,14 +49,33 @@ export default class Recipes extends BaseComponent {
 
 	render() {
 		let postItem = this.state.map(item => {
-			let $contentItem = document.createElement('div');
-			$contentItem.classList.add('content__items');
-			$contentItem.innerHTML = `
-				<div class="img-item">
+			// let $contentItem = document.createElement('div');
+			// $contentItem.classList.add('content__items');
+			// $contentItem.innerHTML = `
+			// 	<div class="img-item">
+			// 		<img src="${item.image}" alt="img">
+			// 	</div>
+			// 	<div class="para-items">
+			// 		<div class="item-info">
+			// 			<h3>${item.title}</h3>
+			// 			<p>${item.content}</p>
+			// 		</div>
+			// 		<div class="item-comment">
+			// 			<label for="like" class="item"><i class="far fa-heart"></i> <span class="number-like">0</span></label>
+			// 			<input type="checkbox" id="like" class="item">
+			// 			<span id="comment" class="item"><i class="fas fa-comments"></i><span class="number-like">0</span></span>
+			// 			<span id="view" class="item"><i class="fas fa-eye"></i><span class="number-like">0</span></span>
+			// 		</div>
+			// 	</div>
+			// `
+            let $contentItem = document.createElement('div');
+			$contentItem.classList.add('card');
+            $contentItem.innerHTML = `
+                <div class="card-img">
 					<img src="${item.image}" alt="img">
 				</div>
-				<div class="para-items">
-					<div class="item-info">
+				<div class="card-content">
+					<div class="card-info">
 						<h3>${item.title}</h3>
 						<p>${item.content}</p>
 					</div>
@@ -67,7 +86,7 @@ export default class Recipes extends BaseComponent {
 						<span id="view" class="item"><i class="fas fa-eye"></i><span class="number-like">0</span></span>
 					</div>
 				</div>
-			`
+            `
 			return $contentItem
 		})
 		return postItem

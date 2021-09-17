5d10a37853b7ab8eb8c547b41ce6ae6276d461ce
Ashimory <47056302+Ashimory@users.noreply.github.com>
2021-09-17 20:28:03 +0700

    rendered info from firebase instead of static demo data

--- js/renderRecipes.js
+++ js/renderRecipes.js
@@ -1,5 +1,6 @@
 import Recipes from "./screen/allRecipes.js";
 
 let _recipes = new Recipes();
-let $display = document.querySelector('.display');
-$display.append(..._recipes.render()); 
+    let $display = document.querySelector('.display');
+    $display.append(...await _recipes.render()); 
+    console.log($display)
--- js/screen/allRecipes.js
+++ js/screen/allRecipes.js
@@ -3,51 +3,15 @@ import BaseComponent from "../components/BaseComponent.js";
 export default class Recipes extends BaseComponent {
 	constructor(props) {
 		super(props);
-		this.state = [
-			{
-				image: './img/img-para/img1.jpg',
-				title: 'Vịt om măng chua nóng hổi nhưng giúp "hạ hỏa", dễ làm ngày giãn cách',
-				content: 'Thịt vịt mát, dễ chế biến, dễ ăn, hợp túi tiền của mọi tầng lớp. Món vịt om măng ngon...',
-			},
-			{
-				image: './img/img-para/img2.jpg',
-				title: 'Những sai lầm nguy hiểm khi ăn thịt gà, cần loại bỏ ngay',
-				content: 'Thịt gà là thực phẩm quen thuộc của mỗi gia đình, tuy nhiên không phải ai cũng biết ăn thịt...',
-			},
-			{
-				image: './img/img-para/img3.jpg',
-				title: 'Thực đơn 4 món ngon bổ dưỡng, giúp tăng cường sức đề kháng vào mùa dịch',
-				content: '4 món này đều rất ngon và phù hợp với khẩu vị của nhiều người, đặc biệt có món trẻ nhỏ rất thích.',
-			},
-			{
-				image: './img/img-para/img4.jpg',
-				title: 'Cách chế biến món ốc len xào dừa trứ danh vùng cực Nam Tổ quốc',
-				content: 'Ốc len xào dừa- món ăn bình dị gắn liền với bữa cơm của người dân lao động nhưng nay...',
-			},
-			{
-				image: './img/img-para/img1.jpg',
-				title: 'Vịt om măng chua nóng hổi nhưng giúp "hạ hỏa", dễ làm ngày giãn cách',
-				content: 'Thịt vịt mát, dễ chế biến, dễ ăn, hợp túi tiền của mọi tầng lớp. Món vịt om măng ngon...',
-			},
-			{
-				image: './img/img-para/img2.jpg',
-				title: 'Những sai lầm nguy hiểm khi ăn thịt gà, cần loại bỏ ngay',
-				content: 'Thịt gà là thực phẩm quen thuộc của mỗi gia đình, tuy nhiên không phải ai cũng biết ăn thịt...',
-			},
-			{
-				image: './img/img-para/img3.jpg',
-				title: 'Thực đơn 4 món ngon bổ dưỡng, giúp tăng cường sức đề kháng vào mùa dịch',
-				content: '4 món này đều rất ngon và phù hợp với khẩu vị của nhiều người, đặc biệt có món trẻ nhỏ rất thích.',
-			},
-			{
-				image: './img/img-para/img4.jpg',
-				title: 'Cách chế biến món ốc len xào dừa trứ danh vùng cực Nam Tổ quốc',
-				content: 'Ốc len xào dừa- món ăn bình dị gắn liền với bữa cơm của người dân lao động nhưng nay...',
-			},
-		]
+		this.state = []
 	}
 
-	render() {
+	async render() {
+		await db.collection("Posts").get().then((querySnapshot) => {
+			querySnapshot.forEach((doc) => {
+				this.state.push(doc.data());
+			});
+		})
 		let postItem = this.state.map(item => {
 			// let $contentItem = document.createElement('div');
 			// $contentItem.classList.add('content__items');
@@ -71,13 +35,11 @@ export default class Recipes extends BaseComponent {
             let $contentItem = document.createElement('div');
 			$contentItem.classList.add('card');
             $contentItem.innerHTML = `
-                <div class="card-img">
-					<img src="${item.image}" alt="img">
-				</div>
+                
 				<div class="card-content">
 					<div class="card-info">
 						<h3>${item.title}</h3>
-						<p>${item.content}</p>
+						<p>${item.subTitle}</p>
 					</div>
 					<div class="item-comment">
 						<label for="like" class="item"><i class="far fa-heart"></i> <span class="number-like">0</span></label>
@@ -90,6 +52,9 @@ export default class Recipes extends BaseComponent {
 			return $contentItem
 		})
 		return postItem
+		// <div class="card-img">
+		// 			<img src="${item.image}" alt="img">
+		// 		</div>
 
 
 
